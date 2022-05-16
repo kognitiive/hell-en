343eaae833c069b70fc6cec785016a9d7d2ba334
Evhenii Danchenko <danchenkoevhenii@gmail.com>
2022-05-17 01:04:02 +0300

    add slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,10 +1,40 @@
-$('.mission-container__list').slick({
+$('.reviews-container__list').slick({
   infinite: true,
   slidesToShow: 1,
   slidesToScroll: 1,
   dots: true,
   prevArrow: '.slider-previous-btn',
   nextArrow: '.slider-next-btn',
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+});
+
+$('.slick-dots').slick({
+  infinite: false,
+  slidesToShow: 7,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        arrows: true,
+        prevArrow: '.dots-previous-btn',
+        nextArrow: '.dots-next-btn',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      }
+    }
+  ]
 });
 
 // $('.mission-container__list').slick({
