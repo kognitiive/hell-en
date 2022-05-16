fdb8229017d24e31236d0a381dc354649c031f0a
ViveeViator <vlad.vm00@gmail.com>
2022-05-16 16:10:42 +0300

    Reviews_1

---
+++ src/js/slider.js
@@ -0,0 +1,16 @@
+$('.mission-container__list').slick({
+  infinite: true,
+  slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    prevArrow: '.slider-previous-btn',
+    nextArrow: '.slider-next-btn',
+});
+
+// $('.mission-container__list').slick({
+//   rtl: true,
+//    prevArrow: '.slider-previous-btn',
+//   nextArrow: '.slider-next-btn',
+// });
+
+// $('.mission-container__iteam').slick();
