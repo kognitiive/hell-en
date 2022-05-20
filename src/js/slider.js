ead8acf298bbf9557c1b4da330d6d12f0457a281
Evhenii Danchenko <danchenkoevhenii@gmail.com>
2022-05-20 22:22:46 +0300

    fixed width

--- src/js/slider.js
+++ src/js/slider.js
@@ -5,36 +5,6 @@ $('.reviews-container__list').slick({
   dots: true,
   prevArrow: '.slider-previous-btn',
   nextArrow: '.slider-next-btn',
-  responsive: [
-    {
-      breakpoint: 767,
-      settings: {
-        arrows: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      }
-    }
-  ]
-});
-
-$('.slick-dots').slick({
-  infinite: false,
-  slidesToShow: 7,
-  slidesToScroll: 1,
-  dots: false,
-  arrows: false,
-  responsive: [
-    {
-      breakpoint: 767,
-      settings: {
-        arrows: true,
-        prevArrow: '.dots-previous-btn',
-        nextArrow: '.dots-next-btn',
-        slidesToShow: 3,
-        slidesToScroll: 1,
-      }
-    }
-  ]
 });
 
 // $('.mission-container__list').slick({
